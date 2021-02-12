e372bcde562e1e16a97ed6d6b4bd1ef6a0648c72
Stevey78 <steve.young78@gmail.com>
2021-02-11 22:28:04 -0500

    no change

---
+++ p_n_p.py
@@ -0,0 +1,20 @@
+# Pride & Prejudice (Jane Austen)
+
+
+# It is a truth universally acknowledged, that a single man in possession
+# of a good fortune must be in want of a wife. However little known the feeling
+# or views of such a man may be on his first entering a neighbourhood, this
+# truth is so well fixed in the minds of the surrounding families, that he is
+# considered as the rightful property of some one or other of their daughters.
+
+import extras as xs
+import random
+
+
+
+
+madlib = f'\nIt is a {noun1} universally acknowledged, that a {adj1} {noun2} in possession\
+\n of a {adj2} {noun3} must be in want of a {noun4}. However {adverb1} known the feeling\
+\n or views of such a man may be on his first entering a neighbourhood, this\
+\n truth is so well fixed in the minds of the surrounding families, that he is\
+\n considered as the rightful property of some one or other of their daughters.\n'
